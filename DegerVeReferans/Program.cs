1446e5509910505069fc78c3b371484698b2dfaf
muttalip591 <muttalip591@BPC13>
2023-12-14 12:05:02 +0300

    Proje dosyası ekle.

---
+++ DegerVeReferans/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerVeReferans
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = sayi1;
+            sayi2 = 50;
+            Console.WriteLine("sayı 1 ={0}", sayi1);
+            Console.WriteLine("sayı 2 ={0}", sayi2);
+            Console.WriteLine("===========================");
+            SayiTutucu sayiTutucu = new SayiTutucu();
+            sayiTutucu.A = 10;
+            SayiTutucu sayiTutucu1 = sayiTutucu;
+            sayiTutucu1.A = 50;
+            Console.WriteLine("sayı tutucu ={0}",sayiTutucu.A);
+            Console.WriteLine("sayı tutucu ={0}",sayiTutucu1.A);
+        }
+    }
+    class SayiTutucu
+    {
+        public int A { get; set; }
+    }
+    
+}
